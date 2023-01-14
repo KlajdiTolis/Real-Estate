0c015ff6c3f7b3459015423a3f4c57b0bcd0eb59
KlajdiTolis <110922024+KlajdiTolis@users.noreply.github.com>
2023-01-14 03:55:12 +0100

    in prg

--- src/pages/home/Loan.tsx
+++ src/pages/home/Loan.tsx
@@ -1,28 +1,71 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { Grid, Button } from '@mui/material/';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { Grid, Button } from "@mui/material/";
+import Typography from "@mui/material/Typography";
 
+import HomeCarousel from "../home/HomeCarousel";
 
 //import images
-import LoanImg from "../../assets/real-estate-business-building-technology.webp"
-import Typography from '@mui/material/Typography';
+import LoanImg from "../../assets/real-estate-business-building-technology.webp";
 
 const Loan = () => {
-    return (
-        <Grid container>
-            <Grid item xs={false} md={6} display={{ lg: "block" }} sx={{ position: "relative", textAlign: "center", top: 200 }}>
-                <Typography sx={{ fontSize: 22, pb: 3, fontWeight: "bold", fontFamily: "monospace", textShadow: "1px 0px #000000" }}>Need a home loan? Get pre-approved</Typography>
-                <Typography sx={{ pb: 3, fontFamily: "monospace" }}>Find a lender who can offer competitive mortgage rates and help you with pre-approval.</Typography>
-                <Button variant="contained" sx={{ fontSize: 18, paddingBlock: 1, borderRadius: 3, mt: 2, mb: 4, boxShadow: 5, bgcolor: "#fffeca", color: "black", fontFamily: "monospace", fontWeight: "bold" }}>
-                    Contact drejtorin
-                </Button>
-            </Grid>
-            <Grid item md={6} display={{ xs: "none", lg: "block" }} sx={{ p: 5, }}>
-                <img src={LoanImg} alt="loan" width={800} height={500} style={{ borderRadius: 100, boxShadow: "1px 2px #000000" }} />
-            </Grid>
-        </Grid>
-    );
-}
+  return (
+    <Grid container>
+      <Grid
+        item
+        xs={false}
+        md={6}
+        display={{ lg: "block" }}
+        sx={{ position: "relative", textAlign: "center", top: 200 }}
+      >
+        <Typography
+          sx={{
+            fontSize: 22,
+            pb: 3,
+            fontWeight: "bold",
+            fontFamily: "monospace",
+            textShadow: "1px 0px #000000",
+          }}
+        >
+          Need a home loan? Get pre-approved
+        </Typography>
+        <Typography sx={{ pb: 3, fontFamily: "monospace" }}>
+          Find a lender who can offer competitive mortgage rates and help you
+          with pre-approval.
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{
+            fontSize: 18,
+            paddingBlock: 1,
+            borderRadius: 3,
+            mt: 2,
+            mb: 4,
+            boxShadow: 5,
+            bgcolor: "#fffeca",
+            color: "black",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+          }}
+        >
+          Contact drejtorin
+        </Button>
+      </Grid>
+      <Grid item md={6} xs={12} display={{ lg: "block" }} sx={{ p: 5 }}>
+        <Box
+          sx={{
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            pt: 10,
+          }}
+        >
+          <HomeCarousel />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
 export default Loan;
