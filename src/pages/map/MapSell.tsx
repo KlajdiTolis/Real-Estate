a903ec44381c64ab1b52847753c19cf132bf244e
KlajdiTolis <klajdi.tolis08@gmail.com>
2023-02-02 17:27:02 +0100

    in prg

--- src/pages/map/MapSell.tsx
+++ src/pages/map/MapSell.tsx
@@ -1,14 +1,12 @@
 import { memo, useMemo, useState, useCallback, FC } from 'react'
-import PropTypes from 'prop-types'
-import { GoogleMap, useLoadScript, MarkerF, Autocomplete } from '@react-google-maps/api'
+import { GoogleMap, useLoadScript, MarkerF, Autocomplete, InfoBox } from '@react-google-maps/api'
 import { Grid, Box } from "@mui/material"
 
 import Places from "./Place"
-
-// interface Props {
-//   lat: any,
-//   lng: any
-// }
+interface Props {
+  lat: any,
+  lng: any
+}
 
 const positions = [{ name: "Tirana", location: { lat: 41.3275, lng: 19.8187 } }, { name: "Vlore", location: { lat: 41.335300, lng: 19.818682 } }, { name: "Vlorsdds", location: { lat: 40.1501, lng: 19.8068 } }, { name: "Vlodsadre", location: { lat: 41.318432, lng: 19.801291 } }, { name: "Vlordsddddde", location: { lat: 41.329896, lng: 19.792880 } }]
 
@@ -28,10 +26,11 @@ const center1 = {
 }
 
 const Map = () => {
+  // const Map:FC<Props> = ({ lat, lng }) => {
 
   const [map, setMap] = useState(/** @type google.maps.Map */(null))
   const [search, setSearch] = useState("")
-  const [selectedMarker, SetSelectedMarker] = useState("")
+  const [selectedMarker, setSelectedMarker] = useState(null);
   const [selected, setSelected] = useState(null);
 
   const { isLoaded } = useLoadScript({
@@ -43,37 +42,42 @@ const Map = () => {
     return <div>Loading...</div>
   }
 
-  console.log(selected)
+  // const panTo = useCallback(() => {
+  // }, [])
+
+  // console.log(lat)
 
   return (
     <Box sx={{ display: "flex", height: "100vh", }}>
-      <Box sx={{ width: "100%", height: "100vh", pt: 10, pl: 5, pb: 5 }}>
-        <div className="places-container">
-          <Places setSelected={setSelected} />
-        </div>
-
-        {/* <button onClick={() => (map as any).panTo(selectedMarker)}>GOO</button> */}
-        <GoogleMap zoom={13} center={center} mapContainerStyle={{ width: "100%", height: "80vh", borderRadius: 30 }}
-          options={{
-            fullscreenControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            disableDefaultUI: false,
-          }}
-          onLoad={(map: any) => setMap(map)}
-        >
-          <Box sx={{ textAlign: "center", pt: 2 }}>
-            {/* <Autocomplete>
-              <input style={{ width: 300, height: 40, backgroundColor: "white", position: "relative", zIndex: 1, borderRadius: 10, paddingLeft: 35, backgroundImage: `url(${"https://static.thenounproject.com/png/101791-200.png"})`, backgroundSize: "30px", backgroundRepeat: "no-repeat", }}
-                type="search" id="search" placeholder="Search..." required onChange={(e) => setSearch(e.target.value)} value={search} />
-            </Autocomplete> */}
-          </Box>
-          {selected && <MarkerF position={selected} />}
-          {/* {positions?.map((marker: any) => (
+      <Grid container>
+        <Grid item xs={6} md={12}>
+          <Box>Filters</Box>
+        </Grid>
+        <Grid item xs={6} md={12}>
+          <Box sx={{ width: "100%", height: "100vh", pt: 10 }}>
+            {/* <button onClick={() => (map as any).panTo(selectedMarker)}>GOO</button> */}
+            <GoogleMap zoom={13} center={center} mapContainerStyle={{ width: "100%", height: "80vh", }}
+              options={{
+                fullscreenControl: false,
+                streetViewControl: false,
+                mapTypeControl: false,
+                disableDefaultUI: false,
+              }}
+              onLoad={(map: any) => setMap(map)}
+            >
+              <Box sx={{ textAlign: "center", pt: 2 }}>
+                <Box sx={{ position: "relative", zIndex: 1 }}>
+                  <Places setSelected={setSelected} />
+                </Box>
+              </Box>
+              {selected && <MarkerF position={selected} />}
+              {/* {positions?.map((marker: any) => (
             <MarkerF position={marker.location} onClick={() => SetSelectedMarker(marker.location)} />
-          ))} */}
-        </GoogleMap>
-      </Box>
+          ))}  */}
+            </GoogleMap>
+          </Box>
+        </Grid>
+      </Grid>
     </Box >
   )
 }
