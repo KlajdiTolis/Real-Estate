f4786c9cf754f8532664c2a8be4f2f594b8675de
KlajdiTolis <klajdi.tolis08@gmail.com>
2023-04-11 15:35:12 +0200

    in prg

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import EditPost from "./pages/posts/EditPost";
 import Buy from "./pages/buy/Buy";
 import Sell from "./pages/sell/Sell";
 import Rent from "./pages/rent/Rent";
+import SignUp from "./layout/SignUp";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="rent" element={<Rent />} />
       <Route path="loan" element={<Rent />} />
       <Route path="dashboard/edit" element={<EditPost />} />
+      <Route path="signUp" element={<SignUp />} />
     </Routes>
   );
 };
---
+++ src/layout/SignUp.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Box, Grid } from "@mui/material"
+
+const SignUp = () => {
+    return (
+        <Grid container sx={{ height: "100vh"}}>
+                <Grid item md={12} sx={{ display: "flex", justifyContent: "center",alignItems: "center" }} >
+                    <Box sx={{ border: 2, width: 200, height: 300 }}>
+                        <Box>
+                        sadsadsads
+                        </Box>
+                        <Box>
+                        sadsadsads
+                        </Box>
+                        <Box>
+                        sadsadsads
+                        </Box>
+                    </Box>
+            </Grid>
+            {/* <Grid item>
+            </Grid> */}
+        </Grid>
+    )
+}
+
+export default SignUp
